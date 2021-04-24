e1aae8a4e53dd8202b9f7b05760e885d7493442d
disaq1 <slava79198015981@yandex.ru>
2021-04-24 14:26:19 +0400

    first commit

---
+++ #src/js/burger.js
@@ -0,0 +1,7 @@
+﻿const toggleBtn = document.querySelector('.burger--btn');
+const headerMenu = document.querySelector('.header__menu');
+
+toggleBtn.onclick = function() {
+    headerMenu.classList.toggle('active');
+}
+console.log(toggleBtn);
