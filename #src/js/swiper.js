57b689d43f07da94013e3d2428dbe7c832ac8fef
disaq1 <slava79198015981@yandex.ru>
2021-04-25 20:25:43 +0400

    Other

--- #src/js/swiper.js
+++
@@ -1,9 +0,0 @@
-﻿var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    slidesPerColumn: 2,
-    spaceBetween: 30,
-    pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    },
-});
